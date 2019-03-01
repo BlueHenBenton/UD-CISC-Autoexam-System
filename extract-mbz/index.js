d7ad07a04cb25d5e688126eeb4d831c136f78894
Zack Klodnicki <klodzack@udel.edu>
2019-02-28 20:00:13 -0500

    Added comments

--- extract-mbz/index.js
+++ extract-mbz/index.js
@@ -2,14 +2,18 @@ const tar = require('tar');
 const mkdirp = require('mkdirp');
 
 async function untar(inPath, outPath) {
+  // Create directory if it doesn't exist.
   mkdirp(outPath, (err) => {
     if (err) throw err;
   });
+  // Extract the input file to that directory.
   await tar.x({
     file: inPath,
     C: outPath
   });
 }
+
+// CLI: convenient now, not necessary in the future.
 
 if (process.argv.length < 3 || process.argv.length > 4) {
   console.error('Please provide 1-2 arguments.');
