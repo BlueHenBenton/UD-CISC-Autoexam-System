d7c0fa74dc53d691450a32233f4827617c47ee7d
Zack Klodnicki <klodzack@udel.edu>
2019-02-28 19:54:42 -0500

    It works

--- extract-mbz/index.js
+++ extract-mbz/index.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+const tar = require('tar');
+
+async function untar(inPath, outPath) {
+  await tar.x({
+    file: inPath,
+    C: outPath
+  });
+}
+
+untar('sample.mbz', 'out').then(() => console.log('Done.'));
