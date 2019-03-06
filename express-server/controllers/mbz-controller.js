9311e12c2fe835ec364a0b4a1ab596296d7a601e
Zack Klodnicki <klodzack@udel.edu>
2019-03-06 13:46:16 -0500

    Express server parses question info

--- express-server/controllers/mbz-controller.js
+++ express-server/controllers/mbz-controller.js
@@ -2,13 +2,22 @@ const awaitTemp = require('../utilities/await-temp');
 const fs = require('fs-extra'); // like node's 'fs', but with more methods and *promise support*.
 const del = require('del');
 const extractMbz = require('../utilities/extract-mbz');
+const parseMetaFromTags = require('../utilities/parse-meta-from-tags');
 
 async function parseAndSaveMbz(req, res) {
   const dir = await awaitTemp.mkdir();
   await fs.writeFile(dir + '/upload.mbz', req.body);
   await extractMbz(dir + '/upload.mbz', dir + '/extracted');
-//  await del(dir + '/**', { force: true });
-  res.send(dir);
+  const questions = await fs.readdir(dir + '/extracted/activities');
+  const questionMetaPromises = questions.map(async (question) => {
+    return {
+      path: question,
+      ...(await parseMetaFromTags(dir + `/extracted/activities/${question}`))
+    };
+  });
+  const questionMetas = await Promise.all(questionMetaPromises);
+  await del(dir, { force: true });
+  res.send(questionMetas);
 }
 
 module.exports = {
