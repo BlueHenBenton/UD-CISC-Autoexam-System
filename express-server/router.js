7b18c31c642bb715435b2b8cb4101601385fdcea
Zack Klodnicki <klodzack@udel.edu>
2019-03-05 19:53:58 -0500

    Added CORS

--- express-server/router.js
+++ express-server/router.js
@@ -1,5 +1,7 @@
+const cors = require('cors');
 const { TestController } = require('./controllers');
 
 module.exports = function(app) {
+  app.use(cors());
   app.get('/test', TestController.test)
 }
