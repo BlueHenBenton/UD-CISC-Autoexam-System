21226d18fdf20dfd526e740fe0df0ddd9e15d3c3
Zack Klodnicki <klodzack@udel.edu>
2019-03-05 22:05:57 -0500

    Copy post'd data into a file in the temporary directory.

--- express-server/controllers/mbz-controller.js
+++ express-server/controllers/mbz-controller.js
@@ -1,9 +1,10 @@
 const awaitTemp = require('../utilities/await-temp');
-const awaitFs = require('await-fs');
+const fs = require('fs-extra'); // like node's 'fs', but with more methods and *promise support*.
 const del = require('del');
 
 async function parseAndSaveMbz(req, res) {
   const dir = await awaitTemp.mkdir();
+  await fs.writeFile(dir + '/upload.mbz', req.body);
   await del(dir + '/**', { force: true });
   res.send(dir);
 }
