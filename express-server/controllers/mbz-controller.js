843af52646e774e555adb8db1135dd86a18843cd
Zack Klodnicki <klodzack@udel.edu>
2019-03-06 13:52:11 -0500

    Parse course info

--- express-server/controllers/mbz-controller.js
+++ express-server/controllers/mbz-controller.js
@@ -3,6 +3,7 @@ const fs = require('fs-extra'); // like node's 'fs', but with more methods and *
 const del = require('del');
 const extractMbz = require('../utilities/extract-mbz');
 const parseMetaFromTags = require('../utilities/parse-meta-from-tags');
+const parseCourse = require('../utilities/parse-course');
 
 async function parseAndSaveMbz(req, res) {
   const dir = await awaitTemp.mkdir();
@@ -12,12 +13,17 @@ async function parseAndSaveMbz(req, res) {
   const questionMetaPromises = questions.map(async (question) => {
     return {
       path: question,
-      ...(await parseMetaFromTags(dir + `/extracted/activities/${question}`))
+      ...(await parseMetaFromTags(`${dir}/extracted/activities/${question}`))
     };
   });
   const questionMetas = await Promise.all(questionMetaPromises);
+  const courseMeta = await parseCourse(`${dir}/extracted`);
+  const response = {
+    questions: questionMetas,
+    course: courseMeta
+  };
   await del(dir, { force: true });
-  res.send(questionMetas);
+  res.send(response);
 }
 
 module.exports = {
