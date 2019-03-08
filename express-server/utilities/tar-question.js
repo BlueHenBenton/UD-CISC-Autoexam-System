572bdbf30492c38fa7980876b67d2f6d18fc3644
Zack Klodnicki <klodzack@udel.edu>
2019-03-07 19:43:08 -0500

    Tar the file and store in a permanent place

---
+++ express-server/utilities/tar-question.js
@@ -0,0 +1,28 @@
+const tar = require('tar');
+const mkdirp = require('mkdirp');
+
+module.exports = async function (questionPath, outDir) {
+  const splitPath = questionPath.split('/');
+  const name = splitPath[splitPath.length - 1];
+  const outFile = outDir + '/' + name + '.tar.gz';
+
+  // Make the folder if it doesn't exist
+  await new Promise((resolve, reject) => {
+    mkdirp(outDir, (err, made) => {
+      if (err) reject(err);
+      else resolve(made);
+    });
+  });
+
+  // Create the archive 
+  await tar.c(
+    {
+      gzip: true,
+      file: process.cwd() + '/' + outFile,
+      cwd: splitPath.filter((x, i) => i < splitPath.length - 1).join('/')
+    },
+    [name]
+  );
+
+  return outFile;
+}
