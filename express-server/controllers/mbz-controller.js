80de6c3c2414737af0461902c8e773fc36f97fa8
Zack Klodnicki <klodzack@udel.edu>
2019-03-07 19:58:01 -0500

    Added comments!

--- express-server/controllers/mbz-controller.js
+++ express-server/controllers/mbz-controller.js
@@ -8,26 +8,37 @@ const parseCourse = require('../utilities/parse-course');
 const tarQuestion = require('../utilities/tar-question');
 
 async function parseAndSaveMbz(req, res) {
+  // Create a temporary directory
   const dir = await awaitTemp.mkdir();
-  console.log('Temp dir: ' + dir);
+  // Put the mbz in that directory
   await fs.writeFile(dir + '/upload.mbz', req.body);
+  // Extract the mbz
   await extractMbz(dir + '/upload.mbz', dir + '/extracted');
+  // What questions are here?
   const questions = await fs.readdir(dir + '/extracted/activities');
+  // Create an array of promises that all result in question metadata
   const questionMetaPromises = questions.map(async (question) => {
+    // Tar this question and put it in the permanent storage
     await tarQuestion(`${dir}/extracted/activities/${question}`, config.saveDirectory);
+
     return {
       path: question,
       ...(await parseMetaFromTags(`${dir}/extracted/activities/${question}`)),
       tarpath: `storage/activities/${question}.tar.gz`
     };
   });
+  // Await all of those promises
   const questionMetas = await Promise.all(questionMetaPromises);
+  // Parse the course metadata
   const courseMeta = await parseCourse(`${dir}/extracted`);
+  // Formulate the response
   const response = {
     questions: questionMetas,
     course: courseMeta,
   };
+  // Remove the temporary directory
   await del(dir, { force: true });
+  // Send the response
   res.send(response);
 }
 
