1718bfa1e55d8cabc8dbde45b91c9ae978e92180
Zack Klodnicki <klodzack@udel.edu>
2019-05-01 03:01:41 -0400

    Any key in mongodb works

--- utilities/is-valid-api-key.js
+++ utilities/is-valid-api-key.js
@@ -1,9 +1,10 @@
 const config = require('./config');
+const ApiKey = require('../models/api-key');
 
 async function isValidApiKey(key) {
     if (key === config.superuserApiKey) return true;
 
-    return false;
+    return (await ApiKey.findOne({ key: key })) !== null;
 }
 
 module.exports = isValidApiKey;
