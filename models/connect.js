a538dcb76eb0c533d0ddda64d22c43d5259429b2
Zack Klodnicki <klodzack@udel.edu>
2019-05-23 22:13:50 -0400

    Mongodb connection string stored in config.json

--- models/connect.js
+++ models/connect.js
@@ -4,6 +4,4 @@
 const mongoose = require('mongoose');
 const config = require('../utilities/config.js');
 
-const url = `mongodb://localhost:27017/${config.dbName}`;
-
-mongoose.connect(url, { useNewUrlParser: true });
+mongoose.connect(config.connectionString, { useNewUrlParser: true });
