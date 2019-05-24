ae8e915bd1078c7ee6345989e1ec81e509498fbe
Zack Klodnicki <klodzack@udel.edu>
2019-05-23 22:51:27 -0400

    Fix bug

--- utilities/upload-document.js
+++ utilities/upload-document.js
@@ -1,27 +1,17 @@
-var mongo = require('mongodb');
 const config = require('../utilities/config');
-const MongoClient = require('mongodb').MongoClient;
-
-// This is the name of the mongo database.
-const DBNAME = config.dbName || 'group9db';
-// This is the name of the collection within the database
-const COLLECTIONNAME = config.questionsCollection || "questions";
-// This is the port and location that mongo is using on the network
-const url = "mongodb://localhost:27017/";
+const { MongoClient } = require('mongodb');
 
 /** Add the given array of documents into the mongodb 'questions' collection. */
 module.exports = function uploadDocuments(documents){
     return new Promise((resolve, reject) => {
         // Connect to the running Mongo instance
-        MongoClient.connect(url, function(err, db){
+        MongoClient.connect(config.connectionString, function(err, connection) {
             if(err) reject(err);
-            // Connect to the proper database
-            var dbo = db.db(DBNAME);
             // Insert the documents into the collection
-            dbo.collection(COLLECTIONNAME).insertMany(documents, function(err, res){
+            connection.db().collection(config.questionsCollection).insertMany(documents, function(err, res){
                 if (err) reject(err);
                 console.log("Inserted Documents");
-                db.close();
+                connection.close();
                 resolve();
             });
         });
